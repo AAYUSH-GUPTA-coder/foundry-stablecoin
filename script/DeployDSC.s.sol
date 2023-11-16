a8909cfc45d5fadb2ab931dadd673e2467f072b0
Aayush Gupta <guptaaayush432@gmail.com>
2023-11-16 17:02:40 +0530

    updated DeployDSC.s.sol

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -4,14 +4,39 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
 import {DSCEngine} from "../src/DSCEngine.sol";
-
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployDSC is Script {
-    function run() external returns(DecentralizedStableCoin, DSCEngine){
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
+    function run() external returns (DecentralizedStableCoin, DSCEngine) {
+        HelperConfig config = new HelperConfig();
+
+        (
+            address wethUsdPriceFeed,
+            address wbtcUsdPriceFeed,
+            address weth,
+            address wbtc,
+            uint256 deployerKey
+        ) = config.activeNetworkConfig();
+
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
         vm.startBroadcast();
+        // deployed Decentraised stable coin
         DecentralizedStableCoin dsc = new DecentralizedStableCoin();
-        DSCEngine engine = new DSCEngine();
-        
+        // deployed Decentraised stable coin Engine
+        DSCEngine engine = new DSCEngine(
+            tokenAddresses,
+            priceFeedAddresses,
+            address(dsc)
+        );
+        // Transfer ownership of DSC to engine
+        dsc.transferOwnership(address(engine));
+
         vm.stopBroadcast();
+        return (dsc, engine);
     }
 }
