bec5e072aed86b6d056a02308f20529e0c61a24c
Aayush Gupta <guptaaayush432@gmail.com>
2023-11-16 17:25:46 +0530

    return HelperConfig config

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -40,6 +40,6 @@ contract DeployDSC is Script {
         dsc.transferOwnership(address(engine));
 
         vm.stopBroadcast();
-        return (dsc, engine);
+        return (dsc, engine, config);
     }
 }
